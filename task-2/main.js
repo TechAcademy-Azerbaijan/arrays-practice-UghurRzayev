7699ac43a241e96d5638ac8e290c0832eaf0f93f
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-01 22:16:00 +0400

    Task 2 done

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,12 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let numbers = result.input.split(",")
+    let say = 0
+    for (let i = 1; i < numbers.length-1; i++) {
+        if (numbers[i]>numbers[i-1]&&numbers[i]>numbers[i+1]) {
+            say = say + 1
+        }
+    }console.log(say);
   
 });
