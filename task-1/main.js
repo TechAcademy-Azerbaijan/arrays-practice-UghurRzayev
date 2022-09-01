093158663c49ba34053757bba7e0809ab2d745e9
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-01 21:11:41 +0400

    Task 1 done

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,21 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let array = result.input.split(",")
+    let a = 0;
+    let cem = 0
+    let say = 0
+    
+    for (let i = 0; i < array.length; i++) {
+        a = a + parseFloat(array[i])
+    } a = a/array.length
+    for (let j = 0; j < array.length; j++) {
+        const element = array[j];
+        if (array[j]>a) {
+            cem = parseInt(cem)+parseInt(array[j])
+            say++
+        }
+    } console.log(`${cem}\n${say}`);
+    
   
 });
