bb72d7979a15cb097b50f35902934dc0df640d24
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-02 10:30:38 +0400

    Task 3 done

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,22 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    let n = result.input.split(",");
+    let max = parseInt(n[0]);
+    let min = parseInt(n[0]);
+    let max_index = 0;
+    let min_index = 0;
+    for (i = 0; i < n.length; i++) {
+        const element = parseInt(n[i]);
+        if (element > max) {
+            max = element
+            max_index = i
+        }
+        if (element < min) {
+            min = element
+            min_index = i
+        }
+    }
+    [n[min_index],n[max_index]]=[max,min]
+    console.log(n.join("\n"));
 });
